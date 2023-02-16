d37d66e84075de563cc9fd7b529de6f3d835846a
Aksiniya Stepanova <sxenystep.24@gmail.com>
2023-02-16 22:41:52 +0300

    New change

--- api_yamdb/api/permissions.py
+++ api_yamdb/api/permissions.py
@@ -11,8 +11,10 @@ class ReadOnlyPermission(permissions.BasePermission):
 class AdminOnly(permissions.BasePermission):
     def has_permission(self, request, view):
         return (
-            request.user.is_admin
-            or request.user.is_staff
+            request.user.is_authenticated
+            and (request.user.is_superuser
+                 or request.user.is_staff
+                 or request.user.is_admin)
         )
 
     def has_object_permission(self, request, view, obj):
